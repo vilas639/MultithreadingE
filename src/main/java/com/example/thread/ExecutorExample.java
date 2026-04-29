369c24135c2b9765b9b5425639f15064be06074a
vilas639 <vilasjdhv639@gmail.com>
2026-04-29 14:14:16 +0530

    Update ExecutorExample.java
    excutor service 2 sequenctial service to parrallel

--- src/main/java/com/example/thread/ExecutorExample.java
+++ src/main/java/com/example/thread/ExecutorExample.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 
 public class ExecutorExample {
 
-	public static void main(String[] args) {
+	public static void main1(String[] args) {
 		// TODO Auto-generated method stub
 		
 		//ExecutorService excutor= Executors.newFixedThreadPool(3);
@@ -136,10 +136,96 @@ public class ExecutorExample {
           
 		
 	}
+	
+	public  static void main2(String[] args)
+	{
+		
+		//two sequenctiak api should call in parallel to reduce time 
+		
+		long startTime = System.currentTimeMillis(); // start
+
+		System.out.println(findMultiplecation(4));
+		
+		
+		System.out.println(findMultiplecation(5));
+		
+		System.out.println("hi");
+		
+		 long endTime = System.currentTimeMillis(); // end
+		 
+		 long diff = endTime - startTime;
+
+	        System.out.println("Start Time: " + startTime);
+	        System.out.println("End Time: " + endTime);
+	        System.out.println("Execution Time: " + diff + " ms");
+		
+	}
+	
+	
+	
+	public  static void main(String[] args)
+	{
+		
+		//two sequenctiak api should call in parallel to reduce time 
+		
+		long startTime = System.currentTimeMillis(); // start
+		
+		ExecutorService e= Executors.newFixedThreadPool(2);
+
+	 Future<String> res1 =	(Future<String>) e.submit( new Runnable() {
+			public void run() {
+				System.out.println(findMultiplecation(4));
+			}
+		});
+	 
+	 Future<String> res2 =	(Future<String>) e.submit( new Runnable() {
+			public void run() {
+				System.out.println(findMultiplecation(5));
+			}
+		});
+		
+		
+	   try {
+		res1.get();
+		res2.get();
+		
+	} catch (InterruptedException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	} catch (ExecutionException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+	  finally {
+		e.shutdown();
+	}
+		
+		System.out.println("hi");
+		
+		 long endTime = System.currentTimeMillis(); // end
+		 
+		 long diff = endTime - startTime;
+
+	        System.out.println("Start Time: " + startTime);
+	        System.out.println("End Time: " + endTime);
+	        System.out.println("Execution Time: " + diff + " ms");
+	        
+	        
+		
+	}
+	
 
 	
 	static int findMultiplecation(int number)
 	{
+		try
+		{
+			Thread.sleep(5000);
+		}
+		catch(InterruptedException e)
+		{
+			e.printStackTrace();
+		}
 	  return number*number;
 	}
 }
